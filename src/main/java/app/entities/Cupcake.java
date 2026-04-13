0442421353bdf486e32c9eefb6ea8dc90317e18f
Nakatina <qwafel1122@gmail.com>
2026-04-13 10:32:11 +0200

    Created entity Cupcake

--- src/main/java/app/entities/Cupcake.java
+++ src/main/java/app/entities/Cupcake.java
@@ -1,4 +1,27 @@
 package app.entities;
 
 public class Cupcake {
+    private CupcakeBottom bottom;
+    private CupcakeTop top;
+    private int id;
+
+    public Cupcake(int id, CupcakeBottom bottom, CupcakeTop top,){
+        this.bottom = bottom;
+        this.top = top;
+        this.id;
+    }
+    public Cupcake(CupcakeBottom bottom, CupcakeTop top,){
+        this.bottom = bottom;
+        this.top = top;
+
+    }
+
+    public int getCupcakeBottom(){
+        return bottom;
+    }
+    public  setCupcakeBottom(CupcakeBottom bottom){
+        this.bottom;
+    }
+
+
 }
